6d9c88bd127fd68e94a845986da2bbd71c28e186
Talgat Muratbekov <talgat2307@gmail.com>
2020-11-04 20:14:32 +0600

    Updated index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const artists = require('./app/artists');
 const albums = require('./app/albums');
 const tracks = require('./app/tracks');
+const users = require('./app/users');
+const track_history = require('./app/track_history')
 const app = express();
 const port = 8000;
 
@@ -10,11 +12,14 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const run = async () => {
-  await mongoose.connect('mongodb://localhost/playlist', {useNewUrlParser: true, useUnifiedTopology: true});
+  await mongoose.connect('mongodb://localhost/playlist',
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
   app.use('/artists', artists);
-  app.use('/albums', albums );
-  app.use('/tracks', tracks );
+  app.use('/albums', albums);
+  app.use('/tracks', tracks);
+  app.use('/users', users);
+  app.use('/track_history', track_history);
 
   console.log('Connected to MongoDB');
   app.listen(port, () => console.log('Server started'));
